b9cc63cbf8dd9e5470302ddec073289e9e9048ac
Ingvild Ødegård <inod@statoil.com>
2015-04-23 12:44:28 +0000

    Add is now working

--- client/scripts/controllers/trips.js
+++ client/scripts/controllers/trips.js
@@ -1,6 +1,7 @@
 function TripController($scope){
 	$scope.date = new Date();
 	$scope.trips = [];
+	
 	$scope.delete = function (idx) {
         var i = $scope.trips[idx]; 
         $.getJSON('/trip/deletetrip/'+i._id, function(data){
@@ -9,12 +10,24 @@ function TripController($scope){
         });
     };
     
-    $scope.addtrip = function (idx) {
-      var i = $scope.trips[idx]; 
-        $.getJSON('/trip/deletetrip/'+i._id, function(data){
-        	$scope.trips.removeById(i._id);
-        	$scope.$digest();
+    	
+    
+    $scope.addtrip = function (trip) {
+        $.post('/trip/addtrip/',trip, function(data){
+        	alert("Response: " + data);
         });
+        
+      /*  $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            data: trip,
+			url: '/trip/addtrip/'
+        }).done(function( response ) {
+            if (response.msg === '') {}
+            else {
+                alert('Error: ' + response.msg);
+            }
+        });*/
     };
 
 	$.getJSON('/trip/all', function(data){
